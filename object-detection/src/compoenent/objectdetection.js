80cfbde74e7c5a8de86167a2c126707ce307864b
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-09-14 13:47:36 +0530

    Thief DetectionAlarm

---
+++ object-detection/src/compoenent/objectdetection.js
@@ -0,0 +1,139 @@
+"use client";
+
+import React, {useEffect, useRef, useState} from "react";
+import Webcam from "react-webcam";
+import {load as cocoSSDLoad} from "@tensorflow-models/coco-ssd";
+import * as tf from "@tensorflow/tfjs";
+
+let detectInterval;
+
+const ObjectDetection = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  async function runCoco() {
+    setIsLoading(true);
+    const net = await cocoSSDLoad();
+    setIsLoading(false);
+
+    detectInterval = setInterval(() => {
+      runObjectDetection(net);
+    }, 10);
+  }
+
+  async function runObjectDetection(net) {
+    if (
+      canvasRef.current &&
+      webcamRef.current !== null &&
+      webcamRef.current.video?.readyState === 4
+    ) {
+      canvasRef.current.width = webcamRef.current.video.videoWidth;
+      canvasRef.current.height = webcamRef.current.video.videoHeight;
+
+      const detectedObjects = await net.detect(
+        webcamRef.current.video,
+        undefined,
+        0.6
+      );
+
+
+      const context = canvasRef.current.getContext("2d");
+      renderPredictions(detectedObjects, context);
+    }
+  }
+
+  const showmyVideo = () => {
+    if (
+      webcamRef.current !== null &&
+      webcamRef.current.video?.readyState === 4
+    ) {
+      const myVideoWidth = webcamRef.current.video.videoWidth;
+      const myVideoHeight = webcamRef.current.video.videoHeight;
+
+      webcamRef.current.video.width = myVideoWidth;
+      webcamRef.current.video.height = myVideoHeight;
+    }
+  };
+
+  useEffect(() => {
+    runCoco();
+    showmyVideo();
+  }, []);
+
+
+
+
+
+const renderPredictions = (predictions, ctx) => {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  const font = "16px sans-serif";
+  ctx.font = font;
+  ctx.textBaseline = "top";
+
+  predictions.forEach((prediction) => {
+    const [x, y, width, height] = prediction["bbox"];
+
+    const isPerson = prediction.class === "person";
+
+    ctx.strokeStyle = isPerson ? "#FF0000" : "#00FFFF";
+    ctx.lineWidth = 4;
+    ctx.strokeRect(x, y, width, height);
+
+    ctx.fillStyle = `rgba(255, 0, 0, ${isPerson ? 0.2 : 0})`; 
+    ctx.fillRect(x, y, width, height);
+
+    ctx.fillStyle = isPerson ? "#FF0000" : "#00FFFF";
+    const textWidth = ctx.measureText(prediction.class).width;
+    const textHeight = parseInt(font, 10); 
+    ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+
+    ctx.fillStyle = "#000000";
+    ctx.fillText(prediction.class, x, y);
+
+    if(isPerson){
+      playAudio()
+    }
+
+    if(!isPerson){
+      StopAudio()
+    }
+
+  });
+};
+
+const playAudio =()=>{
+  const audio = new Audio('./police.mp3');
+  audio.play();
+}
+
+ const StopAudio = () =>{
+  const audio = new Audio('./police.mp3');
+  audio.pause();
+ }
+
+  return (
+    <div className="mt-8">
+      {isLoading ? (
+        <div className="gradient-text">Loading AI Model...</div>
+      ) : (
+        <div className="relative flex justify-center items-center gradient p-1.5 rounded-md">
+          <Webcam
+            ref={webcamRef}
+            className="rounded-md w-full lg:h-[720px]"
+            muted
+          />
+
+          <canvas
+            ref={canvasRef}
+            className="absolute top-0 left-0 z-99999 w-full lg:h-[720px]"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ObjectDetection;
---
+++ object-detection/src/pages/index.js
@@ -0,0 +1,12 @@
+import ObjectDetection from "../compoenent/objectdetection";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-8 bg-black">
+      <h1 className="gradient-title font-extrabold text-3xl md:text-6xl lg:text-8xl tracking-tighter md:px-6 text-center text-white">
+        Thief Detection Alarm
+      </h1>
+      <ObjectDetection />
+    </main>
+  );
+}
